39d8836cddd7d051b23ff3eb4efcdf44ae8a724a
thomasHorvath <116713@fhwn.ac.at>
2022-11-30 20:40:41 +0100

    Test

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -7,10 +7,11 @@ public class Main {
 
     private static String folder = "C:\\Users\\Tomcii\\Desktop\\";
 
-
     public static void main(String[] args) {
 
         doWork("Hello.txt", System.out::println);
+
+
     }
 
     private static void doWork(String path, Consumer<String> consumer) {
