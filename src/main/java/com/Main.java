f17a80767c8ee7a2cd88f17f7514c81be223fdb2
thomasHorvath <116713@fhwn.ac.at>
2022-11-30 20:34:10 +0100

    Init

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.io.*;
+import java.util.function.Consumer;
+
+public class Main {
+
+    private static String folder = "C:\\Users\\Tomcii\\Desktop\\";
+
+
+    public static void main(String[] args) {
+
+        doWork("Hello.txt", System.out::println);
+    }
+
+    private static void doWork(String path, Consumer<String> consumer) {
+        try{
+            File file = new File(folder + path);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+            String line;
+
+                while ((line = bufferedReader.readLine()) != null){
+                    consumer.accept(line);
+                }
+
+            bufferedReader.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
