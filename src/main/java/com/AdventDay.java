a19ea50ec70064e4e6b4e2de6d0085c75864860e
Tomciii <69507140+Tomciii@users.noreply.github.com>
2022-12-01 15:01:35 +0100

    Refactor
    Refactor

--- src/main/java/com/AdventDay.java
+++ src/main/java/com/AdventDay.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AdventDay {
-
+    
     public static void day2(){
-        solveDay2("day2.txt");
+        System.out.println(solveDay1("day2.txt"));
     }
 
     private static void solveDay2(String path) {
@@ -14,13 +14,13 @@ public class AdventDay {
     }
 
     public static void day1() {
-        solveDay1("day1.txt");
+        System.out.println(solveDay1("day1.txt"));
     }
 
-    private static void solveDay1(String path) {
-
-        List<String> input = Util.readFile(path);
+    private static List<Integer> solveDay1(String path) {
         List<Integer> result = new ArrayList<>();
+        List<String> input = Util.readFile(path);
+        List<Integer> calorieList = new ArrayList<>();
         int currentSum = 0;
         int biggestSum = 0;
 
@@ -30,17 +30,19 @@ public class AdventDay {
                     biggestSum = currentSum;
                 }
 
-                result.add(currentSum);
+                calorieList.add(currentSum);
                 currentSum = 0;
             } else {
                 currentSum += Integer.valueOf(line);
             }
         }
 
-        List<Integer> sorted = result.stream().sorted().toList();
+        List<Integer> sorted = calorieList.stream().sorted().toList();
         int biggestThreeSum = sorted.get(sorted.size() - 1) + sorted.get(sorted.size() - 2) + sorted.get(sorted.size() - 3) ;
 
-        System.out.println("Biggest amount: " + biggestSum);
-        System.out.println("Sum of biggest 3: " + biggestThreeSum );
+        result.add(biggestSum);
+        result.add(biggestThreeSum);
+
+        return result;
     }
 }
--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,10 +3,6 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        // Day 1
         AdventDay.day1();
-
-        // Day 2
-        AdventDay.day2();
     }
 }
