2472c3316a0407dedf31da1ac3393570d757a168
Tomciii <69507140+Tomciii@users.noreply.github.com>
2022-12-02 12:33:28 +0100

    Day 2 Done
    Day 2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # AdventOfCode-2022
 AdventOfCode 2022
 
-Day 1: 100% Done
+Day 1: Done
+Day 2: Done
--- src/main/java/com/AdventDay.java
+++ src/main/java/com/AdventDay.java
@@ -4,13 +4,90 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AdventDay {
-    
+
     public static void day2(){
-        System.out.println(solveDay1("day2.txt"));
+        System.out.println(solveDay2_2("day2.txt"));
     }
 
-    private static void solveDay2(String path) {
+    private static Integer solveDay2_2(String path) {
+        List<String> input = Util.readFile(path);
+        int result = 0;
 
+        for (String line : input){
+            String[] lineInArray = line.split(" ");
+
+            if (lineInArray[1].equals("X")){
+                if (lineInArray[0].equals("A")){
+                    result = result + 3;
+                } else if (lineInArray[0].equals("B")){
+                    result = result + 1;
+                } else if (lineInArray[0].equals("C")){
+                    result = result + 2;
+                }
+            } else if (lineInArray[1].equals("Y")){
+                result = result + 3;
+
+                if (lineInArray[0].equals("A")){
+                    result = result + 1;
+                } else if (lineInArray[0].equals("B")){
+                    result = result + 2;
+                }
+                else if (lineInArray[0].equals("C")){
+                    result = result + 3;
+                }
+            } else if (lineInArray[1].equals("Z")){
+                result = result + 6;
+
+                if (lineInArray[0].equals("B")){
+                    result = result + 3;
+                } else if (lineInArray[0].equals("C")){
+                    result = result + 1;
+                } else if (lineInArray[0].equals("A")){
+                    result = result + 2;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static Integer solveDay2(String path) {
+        List<String> input = Util.readFile(path);
+        int result = 0;
+
+        for (String line : input){
+            String[] lineInArray = line.split(" ");
+
+            if (lineInArray[1].equals("X")){
+                result = result + 1;
+
+                if (lineInArray[0].equals("A")){
+                    result = result + 3;
+                }
+
+                if (lineInArray[0].equals("C")){
+                    result = result + 6;
+                }
+            } else if (lineInArray[1].equals("Y")){
+                result = result + 2;
+
+                if (lineInArray[0].equals("A")){
+                    result = result + 6;
+                } else if (lineInArray[0].equals("B")){
+                    result = result + 3;
+                }
+            } else if (lineInArray[1].equals("Z")){
+                result = result + 3;
+
+                if (lineInArray[0].equals("B")){
+                    result = result + 6;
+                } else if (lineInArray[0].equals("C")){
+                    result = result + 3;
+                }
+            }
+        }
+
+        return result;
     }
 
     public static void day1() {
