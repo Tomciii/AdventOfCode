1498f57de5ffd64bf2cb576a3643cc02ea0d16b2
Tomciii <69507140+Tomciii@users.noreply.github.com>
2022-12-06 23:28:16 +0100

    Day 1 2015
    Day 1 2015

--- src/main/java/com/puzzleSolver/PuzzleSolver2015.java
+++ src/main/java/com/puzzleSolver/PuzzleSolver2015.java
@@ -12,8 +12,16 @@ public class PuzzleSolver2015 {
         int result = 0;
         List<String> input = Util.readFile(path);
 
-        result = result + StringUtils.countOccurrencesOf(input.get(0),"(");
-        result = result - StringUtils.countOccurrencesOf(input.get(0),")");
+        for (int i = 0; i < input.get(0).length(); i++){
+            if (input.get(0).charAt(i) == '('){
+                result = result + 1;
+            } else {
+                result = result - 1;
+                if (result == -1){
+                    return i + 1;
+                }
+            }
+        }
 
         return result;
     };
