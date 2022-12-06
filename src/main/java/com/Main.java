a514f438b72789ea3afbc84afaa106ad3eeca853
Tomciii <69507140+Tomciii@users.noreply.github.com>
2022-12-06 14:41:17 +0100

    Refactor
    Refactor

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,6 +3,6 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        PuzzleResultPrinter.printResult(PuzzleSolver.solveDay6,"day6.txt");
+        PuzzleResultPrinter.printResult(PuzzleSolver.solveDay6, "2022/day6.txt");
     }
 }
