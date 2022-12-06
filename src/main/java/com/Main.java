4e5b8e5fb0d1eabba03b1c8d7591e8c9f0bfbcab
Tomciii <69507140+Tomciii@users.noreply.github.com>
2022-12-06 23:55:24 +0100

    Day 2 2015
    Day 2 2015

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,11 +1,9 @@
 package com;
-
-import com.puzzleSolver.PuzzleSolver2015;
-import com.puzzleSolver.PuzzleSolver2022;
+import com.puzzleSolver.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        PuzzleResultPrinter.printResult(PuzzleSolver2015.solveDay1, "2015/day1.txt");
+        PuzzleResultPrinter.printResult(PuzzleSolver2015.solveDay2, "2015/day2.txt");
     }
 }
--- src/main/java/com/puzzleSolver/PuzzleSolver2015.java
+++ src/main/java/com/puzzleSolver/PuzzleSolver2015.java
@@ -1,12 +1,49 @@
 package com.puzzleSolver;
 
 import com.Util;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.function.Function;
 
 public class PuzzleSolver2015 {
+
+    public static Function<String, Integer> solveDay2 = (path) -> {
+        Integer result = 0;
+
+        List<String> input = Util.readFile(path);
+
+        for (String line : input){
+            String[] dimensions = line.split("x");
+            Integer l = Integer.valueOf(dimensions[0]);
+            Integer w = Integer.valueOf(dimensions[1]);
+            Integer h = Integer.valueOf(dimensions[2]);
+
+            Integer smallestFace1 = l;
+            Integer smallestFace2 = w;
+            Integer smallestFace3 = h;
+
+            if (l > w){
+                smallestFace1 = w;
+            }
+
+            if (w > h){
+                smallestFace2 = h;
+            }
+
+            if (h > l){
+                smallestFace3 = l;
+            }
+
+            Integer side1 = l*w;
+            Integer side2 = w*h;
+            Integer side3 = h*l;
+
+            result = result + l + smallestFace1 + smallestFace2 + smallestFace3 + l*w*h;
+        //    result = result + 2*l*w + 2*w*h + 2*h*l + smallestSide;
+        }
+
+        return result;
+    };
 
     public static Function<String, Integer> solveDay1 = (path) -> {
         int result = 0;
